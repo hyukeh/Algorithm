e616cfcafc0df050500f2a90c701600eb929fd54
hyukeh <80406115+hyukeh@users.noreply.github.com>
2023-03-09 16:18:02 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        answer = n*12000 + k*2000;
+        
+        if(n>=10){
+            return answer -= n / 10 * 2000;
+        }
+        
+        return answer;
+    }
+}
